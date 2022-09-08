69a393874d37776d73fa00e324c731c26a60465d
Larry Botha <lawrenceguy@gmail.com>
2022-09-08 09:22:41 -0500

    feat(scalars): add example code

--- 01-introduction/03-scalars/src/main.rs
+++ 01-introduction/03-scalars/src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+fn main() {
+    let integer: u64 = 5;
+    let decimal: f64 = 1.5;
+    let character: char = 'S';
+    let boolean: bool = true;
+
+    println!("integer: {integer}");
+    println!("decimal: {decimal}");
+    println!("character: {character}");
+    println!("boolean: {boolean}");
+
+    // math operators do not work on integers and floats unless they are
+    // explicitly cast to the matching type
+    let a = 5;
+    let b = 5.0;
+    let sum = f64::from(a) + b;
+    println!("sum: {sum}");
+}
