f184c6042d33f0ad6a5cd14a899c53a2aecd4b03
Larry Botha <lawrenceguy@gmail.com>
2022-10-02 23:58:05 -0500

    feat(lifetimes): add notes and examples

--- 03-memory-and-lifetimes/04-lifetimes/README.md
+++ 03-memory-and-lifetimes/04-lifetimes/README.md
@@ -3,3 +3,139 @@
 https://app.rust-for-js.dev/posts/12-lifetimes/
 
 ## Takeaways
+
+- lifetime refers to how long the memory of a value 'lives'
+- the lifetime of a value is typically its surrounding scope:
+
+  ```rust
+  fn some_fund() {
+    let x = String::from("foo");
+
+    {
+      let y = String::from("bar");
+    } // <= y's lifetime ends here
+  } // <= x's lifetime ends here
+  ```
+
+- lifetime, as with types, can sometimes be inferred by the compiler, and at
+  other times must be provided explicitly
+- _lifetime annotations_ are the means to explicitly define lifetimes
+
+  ```rust
+  struct Thing <'a> {
+    thing_ref: &'a String,
+  }
+
+  let val_to_ref = String::from("foo");
+  let thing_a = Thing {thing_ref: &val_to_ref};
+  let thing_b = Thing {thing_ref: &val_to_ref};
+  ```
+
+  Each `Thing` instance contains a reference to another value. The lifetime of
+  each instance of `Thing` is now not only the scope where it is instantiated,
+  but also the lifetime of the value it references. Memory allocated to each
+  `Thing` instance cannot be deallocated until it is safe to deallocate the
+  value that `Thing` references
+
+- an instance of a struct that references another value cannot outlive the
+  lifetime of the value it references
+- conventionally, lifetime annotations are written using lowercase single
+  characters
+- lifetime annotations do not change the behaviour of structs - they provide
+  information to the compiler on the lifetime of the dependencies of the
+  struct instances
+- lifetime specifiers for multiple values in a struct can be shared if the
+  values inside the struct instance are defined in the same scope:
+
+  ```rust
+  fn main() {
+    struct Thing<'a> {
+      foo: &'a str,
+      bar: &'a str,
+    }
+
+    // both values are defined in the same scope, so can share the same
+    // lifetime specifier
+    let thing = Thing {
+      foo: "hey",
+      bar: "there",
+    };
+  }
+  ```
+
+- if a return value of a function is derived from a parameter to the function,
+  and Rust cannot infer the lifetime of the return value, the function needs a
+  lifetime annotation:
+
+  ```rust
+  fn my_func'a(key: &str, hash_map: &'a HashMap<&str, &str>) -> &'a str {
+    hash_map.get(key).expect("Key not found")
+  }
+  ```
+
+### Outlives
+
+- in some cases it may be important to specify that one value passed to a
+  function should outlive another. This can be done using the `where` keyword:
+
+  ```rust
+  fn my_func<'a, 'b>(value_a: &'a str, value_b: &'b str) -> &'b str where 'b: 'a', {
+    // ...
+  }
+  ```
+
+  `where 'b: 'a` translates to _where 'b outlives 'a_
+
+### `'static`
+
+- a `'static` keyword exists which indicates that a value's lifetime is the
+  running duration of the application - its lifetime only ends when the
+  application ends
+
+  ```rust
+  static VERSION: u8 = 1;
+  ```
+
+  `VERSION` has the `'static` lifetime. If a static lifetime annotation were
+  required in the context of a struct, the `'static` syntax would be required
+
+### Option
+
+- `Option` is an enum with 2 variants:
+  - `Some` - akin to Haskell's `Just`
+  - `None`
+- the type of an `Option` is provided in a similar manner to how generics are
+  provided to types in `TypeScript`:
+
+  ```rust
+  struct Thing {
+  optional_value: Option<String>,
+  }
+
+  let thing = Thing { optional_value: Some(String::from("foo")) };
+  let no_thing = Thing { optional_value: None) };
+  ```
+
+- `Some` and `None` are actually `Option::Some` and `Option::None`, but Rust
+  includes the values in the prelude by default
+
+### Hashmap
+
+- hashmaps are similar to `Map` in Javascript, except that all key-value pairs
+  must follow the same type definition
+- `HashMap` is imported via `std::collections`:
+
+  ```rust
+  use std::collections::HashMap;
+
+  let hash_map = HashMap::from([( "my_key", String::from("foo") )]);
+  ```
+
+  `HashMap`s instantiated from the `from` associated function require a slice of
+  key-value tuples
+
+- values in a `HashMap` are obtained using the `.get` method, which returns an
+  `Option`
+- `my_hash_map.get(key).expect("Some message")` will kill the running process -
+  `.expect` in Rust should not be used in production code, unless you have a
+  very good reason to kill the process
--- 03-memory-and-lifetimes/04-lifetimes/src/main.rs
+++ 03-memory-and-lifetimes/04-lifetimes/src/main.rs
@@ -1,3 +1,147 @@
+use std::collections::HashMap;
+
+fn simple_lifetimes() {
+    let x = String::from("foo");
+
+    {
+        let y = String::from("bar");
+        println!("y's lifetime ends at the end of this block: {y}");
+    }
+
+    println!("x's lifetime ends at the end of this function: {x}");
+    println!();
+}
+
+fn lifetimes_with_references() {
+    #[derive(Debug)]
+    struct Thing<'a> {
+        //       [1]
+        thing_ref: &'a String,
+        //         [2]
+    }
+
+    fn create_thing(value: &String) -> Thing {
+        Thing { thing_ref: value }
+    }
+
+    let x = String::from("foo");
+    let thing_a = create_thing(&x);
+    let thing_b = create_thing(&x);
+    //    [3]
+
+    // 1 - we indicate that Thing contains a lifetime specified - there is
+    //      some value in Thing that has a lifetime which cannot be inferred
+    //      ny the compiler
+    // 2 - the thing that has a lifetime that can't be inferred is thing_ref
+    // 3 - the lifetimes of these Thing instances are no longer tied only to
+    //      scope of their containing function - they are also tied to the
+    //      lifetime of the scope of x - this is where the need for explicitly
+    //      annotating lifetimes comes in
+
+    println!("x: {x}");
+    println!("thing_a.ref: {:?}", thing_a.thing_ref);
+    println!("thing_b.ref: {:?}", thing_b.thing_ref);
+    println!();
+}
+
+fn intro_to_option() {
+    #[derive(Debug)]
+    struct Person {
+        name: Name,
+    }
+    #[derive(Debug)]
+    struct Name {
+        first_name: String,
+        middle_name: Option<String>,
+        last_name: String,
+    }
+
+    let people = [
+        Person {
+            name: Name {
+                first_name: String::from("Joe"),
+                middle_name: Some(String::from("Monkey")),
+                last_name: String::from("Soap"),
+            },
+        },
+        Person {
+            name: Name {
+                first_name: String::from("Jane"),
+                middle_name: None,
+                last_name: String::from("Does"),
+            },
+        },
+    ];
+
+    println!("{people:?}\n");
+}
+
+fn multiple_lifetimes() {
+    #[derive(Debug)]
+    // both values inside this struct can use the same lifetime specifier as long
+    // as the values are defined in the same scope when instantiating an instance
+    struct Person<'a> {
+        name: &'a str,
+        location: &'a str,
+    }
+
+    fn get_city<'a>(code: &str, airport_codes: &'a HashMap<&str, &str>) -> &'a str {
+        //      [1]                             ]2]                         [3]
+        // 1 - we add a lifetime annotation to the function - the return value of
+        //      this function has a lifetime defined outside of the scope of this
+        //      function
+        // 2 - the parameter where the returned value is obtained from is the
+        //      hash map - the Rust compiler can now link the return value's
+        //      lifetime with the value that it is derived from
+        // 3 - the return value must have the same lifetime annotation - this is
+        //      the value that has a lifetime which has been defined outside of
+        //      this function
+
+        // 'expects' is used hhere as a convenience - don't use it in production
+        // unless you have good readon to end the running application process
+        airport_codes.get(code).expect("Unknown airport code")
+    }
+
+    let airport_codes = HashMap::from([
+        ("PIE", "St. Petersburg"),
+        ("LHR", "London"),
+        ("BOM", "Mumbai"),
+    ]);
+    // PIE exists, so Some is returned
+    println!("PIE: {:?}", airport_codes.get("PIE"));
+    // FOO does not exist, so None is returned
+    println!("PIE: {:?}", airport_codes.get("FOO"));
+    println!();
+
+    let person = Person {
+        name: "Joe Soap",
+        location: "PIE",
+    };
+
+    println!("{:?}", person);
+    println!(
+        "Welcome {} from {}",
+        person.name,
+        get_city(person.location, &airport_codes)
+    );
+    println!();
+}
+
+fn static_lifetime() {
+    static VERSION: u8 = 1;
+
+    println!("version: {VERSION} - a static!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    simple_lifetimes();
+    lifetimes_with_references();
+
+    // option primer
+    intro_to_option();
+
+    // multiple lifetimes
+    multiple_lifetimes();
+
+    static_lifetime();
 }
